d20e976dde2ba9a4f09705b91d91e07d743cf943
Ira <googirlsb@gmail.com>
2025-11-19 22:33:09 +0700

    4.6

--- src/Basket.java
+++ src/Basket.java
@@ -1,9 +1,30 @@
 public class Basket {
     private  String items = "";
     private int totalPrise = 0;
+    private int limit;
+
+    public Basket() {
+        items = "Spisok tovarov";
+        this.limit = 1000000;
+    }
+
+    public Basket(int limit) {
+        this();
+        this.limit = limit;
+    }
+
+    public Basket(String items, int totalPrise){
+        this();
+        this.items =  this.items + items;
+        this.totalPrise = totalPrise;
+    }
+
 
     public void add(String name, int price) {
         if (contains(name)) {
+            return;
+        }
+        if (totalPrise+price >= limit){
             return;
         }
         items = items + "\n" + name + " - " + price;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Basket vasyaBasket = new Basket();
+        Basket vasyaBasket = new Basket(100);
         vasyaBasket.add("hui", 90);
         vasyaBasket.add("smazka", 50);
 
@@ -9,7 +9,11 @@ public class Main {
         petyaBasket.add("niger", 1000 );
         petyaBasket.add("wother",10);
 
+        Basket mashaBasket = new Basket("stol",500);
+
         vasyaBasket.print("korzina Vasyi");
         petyaBasket.print("Korzina Petyi");
+        mashaBasket.print("korzina Mashi");
+
     }
 }
